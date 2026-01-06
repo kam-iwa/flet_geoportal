ff8d5ecce9c9685ca1dcccee0d503fa80a3692d5
unknown <iwaniukkamil@gmail.com>
2026-01-06 15:50:08 +0100

    added readme, manage.py file and added simple buttons for testing

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -7,8 +7,7 @@ def main(page: ft.Page):
     page.title = "Flet Geoportal"
     page.padding = 20
 
-    page.add(
-        ftm.Map(
+    map_view = ftm.Map(
             expand=True,
             initial_center=ftm.MapLatitudeLongitude(51.9189046, 19.1343786),
             initial_zoom=7,
@@ -19,12 +18,20 @@ def main(page: ft.Page):
                 ),
                 ftm.SimpleAttribution(
                     text="OpenStreetMap Contributors",
-                    alignment=ft.Alignment.BOTTOM_RIGHT,
+                    alignment=ft.Alignment.TOP_RIGHT,
                     on_click=lambda e: asyncio.create_task(ft.UrlLauncher().launch_url("https://openstreetmap.org/copyright")),
                 ),
             ]
+        )
+
+    page.add(
+        ft.Text("Flet Geoportal", size=48),
+        ft.ListView(
+            controls = [ft.Row(controls = [ft.Button(f"Przycisk {i}") for i in range(1,6)])]
         ),
+        map_view
+
     )
 
 if __name__ == "__main__":
-    ft.run(main, view=ft.AppView.WEB_BROWSER, port=8550)
+    ft.run(main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=8550)
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Flet Geoportal
+Simple full-stack geoportal web app written in Python: front-end with using Flet framework, back-end with using FastAPI framework and PostgreSQL + PostGIS database
+
+## How to run?
+To run Flet Geoportal, you need just to input into terminal command `python main.py 'parameter'`. List of available parameters with description of each parameter:
+- `up` - runs the Docker containers of API
+- `build` - builds Docker containers of API
+- `restart` - restarts Docker containers of API
+- `down` - shuts down Docker container of API
